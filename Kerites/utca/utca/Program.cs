63fc46d593e5c4bb9068b9f1111268bf1180f5a1
Székely Szabolcs <szekely.szabolcs_2022b@bolyai-szakkozep.hu>
2026-01-13 18:28:11 +0100

    ALAP KÓD

---
+++ Kerites/utca/utca/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace utca
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //  1. Feladat
+
+            Console.WriteLine("1. Feladat");
+
+            //  2. Feladat
+
+            Console.WriteLine("2. Feladat");
+
+            //  3. Feladat
+
+            Console.WriteLine("3. Feladat");
+
+            //  4. Feladat
+
+            Console.WriteLine("4. Feladat");
+
+            //  5. Feladat
+
+            Console.WriteLine("5. Feladat");
+
+            //  6. Feladat
+
+            Console.WriteLine("6. Feladat");
+
+            //  7. Feladat
+
+            Console.WriteLine("7. Feladat");
+        }
+    }
+}
---
+++ Kerites/utca/utca/Telek.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace utca
+{
+    internal class Telek
+    {
+        // Properties   -   A property egy olyan publikus tulajdonság, amelyen keresztül biztonságosan érjük el és módosítjuk az osztály belső (privát) adatait get és set segítségével.
+        //                  (A C# fordító automatikusan létrehoz egy privát mezőt)
+        public int Oldal { get; set; }
+        public int Szelesseg { get; set; }
+        public int Kerites { get; set; }
+
+        //  Konstruktor
+        public Telek(int oldal, int szelesseg, char kerites)
+        {
+            //  Bal oldalon: a property-k, Jobb oldalon: a konstruktor paraméterei
+            //  A this kulcsszó az osztály aktuális példányára (objektumra) való hivatkozásra szolgál.
+            this.Oldal = oldal;
+            this.Szelesseg = szelesseg;
+            this.Kerites = kerites;
+        }
+
+        //  Függvények (Methods)
+
+    }
+}
